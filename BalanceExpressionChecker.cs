2436490480563e0435b0925ee469d54aaf488267
armin <a.shoeibi@alibaba.ir>
2021-11-03 14:53:13 +0330

    Add project files.

---
+++ BalanceExpressionChecker.cs
@@ -0,0 +1,29 @@
+﻿namespace BalancedExpressionsUsingStack;
+
+public class BalanceExpressionChecker
+{
+    private readonly List<char> leftBrackets = new() { '(', '[', '<', '{' };
+    private readonly List<char> rightBrackets = new() { ')', ']', '>', '}' };
+    public bool IsBalanced(string input)
+    {
+        Stack<char> stack = new();
+        foreach (char ch in input)
+        {
+            if (IsLeftBracket(ch))
+                stack.Push(ch);
+            if (IsRightBracket(ch))
+            {
+                if (stack.Count == 0) return false;
+
+                char top = stack.Pop();
+                if (BracketsMatch(top, ch) is false) return false;
+            }
+
+        }
+        return stack.Count == 0;
+
+        bool IsLeftBracket(char ch) => leftBrackets.Contains(ch);
+        bool IsRightBracket(char ch) => rightBrackets.Contains(ch);
+        bool BracketsMatch(char left, char right) => leftBrackets.IndexOf(left) == rightBrackets.IndexOf(right);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using BalancedExpressionsUsingStack;
+
+BalanceExpressionChecker balanceExpressionChecker = new();
+
+string str1 = "(( 1 + 2>";
+bool isBalanced1 = balanceExpressionChecker.IsBalanced(str1);
+Console.WriteLine($"Is {str1} Balanced: {isBalanced1}");
+
+string str2 = "(( 1 + 2)<>";
+bool isBalanced2 = balanceExpressionChecker.IsBalanced(str2);
+Console.WriteLine($"Is {str2} Balanced: {isBalanced2}");
+
+
+string str3 = "<> ([1] + 2)";
+bool isBalanced3 = balanceExpressionChecker.IsBalanced(str3);
+Console.WriteLine($"Is {str3} Balanced: {isBalanced3}");
+
+string str4 = "<> (1) + (2)";
+bool isBalanced4 = balanceExpressionChecker.IsBalanced(str4);
+Console.WriteLine($"Is {str4} Balanced: {isBalanced4}");
